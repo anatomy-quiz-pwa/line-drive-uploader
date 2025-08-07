82e797c15c8c4058dc225e7286d5829317963e47
Bao Bao C <baobaoc@MacBook-Pro.local>
2025-08-07 22:07:49 +0800

    Fix incorrect reference to GOOGLE_CREDENTIALS_JSON

--- drive_uploader.py
+++ drive_uploader.py
@@ -1,15 +1,11 @@
-import os
-import json
-from typing import Optional, Dict
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
-from googleapiclient.errors import HttpError
-import config
+from config import GOOGLE_SERVICE_ACCOUNT_JSON, GOOGLE_DRIVE_FOLDER_ID
 import mimetypes
 
 credentials = service_account.Credentials.from_service_account_file(
-    config.GOOGLE_CREDENTIALS_JSON,
+    GOOGLE_SERVICE_ACCOUNT_JSON,
     scopes=["https://www.googleapis.com/auth/drive"]
 )
 drive_service = build('drive', 'v3', credentials=credentials)
@@ -18,7 +14,7 @@ def upload_file_to_drive(file_path, file_name):
     mime_type = mimetypes.guess_type(file_path)[0] or 'application/octet-stream'
     file_metadata = {
         'name': file_name,
-        'parents': [config.UPLOAD_FOLDER_ID]
+        'parents': [GOOGLE_DRIVE_FOLDER_ID]
     }
     media = MediaFileUpload(file_path, mimetype=mime_type)
     file = drive_service.files().create(body=file_metadata, media_body=media, fields='id, webViewLink').execute()
